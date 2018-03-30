a2be8726f2bd046b5b20fbf7986cfcf894af9b10
Nick Scialli <nscialli@ggti.com>
2018-03-30 14:06:35 -0400

    Add hostname to headline feed

--- client/src/app.js
+++ client/src/app.js
@@ -30,7 +30,11 @@ export class App {
       'pilots',
       'pilot',
       'just',
-      'something'
+      'something',
+      'safety',
+      'airport',
+      'flight',
+      'flights'
     ];
 
   }
@@ -199,6 +203,25 @@ export class App {
       item.hide = false;
     });
   	this.selected = '';
+  }
+
+  extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
   }
 
 }
